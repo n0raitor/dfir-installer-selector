8781f43b069ebd3175fb99107cea6cd3f75122f6
N. Schmidt <n0raitor@pm.me>
2026-01-16 12:23:41 +0100

    Fix Build Script

--- build-exe.ps1
+++ build-exe.ps1
@@ -1,3 +1,3 @@
 pip install pyinstaller openpyxl
 
-pyinstaller --onefile --windowed --add-data "dfir-installer-selector.xlsx;." --add-data "gui_template.xml;." main.py
+pyinstaller --onefile --windowed --add-data "dfir-installer-selector.xlsx;." --add-data "gui_template.xml;." -n "dfir-installer-selector.exe" main.py
